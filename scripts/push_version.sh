dff685c3b5be909512604268037c3228479a8375
migu <migu@example.com>
2025-01-09 09:06:38 +0000

    v0.1.0

--- scripts/push_version.sh
+++ scripts/push_version.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
-# 设置版本号
-VERSION="v0.0.10"
+# 获取当前版本号
+CURRENT_VERSION=$(cat Cargo.toml | grep '^version =' | sed 's/version = "\(.*\)"/\1/')
+echo "Current version: ${CURRENT_VERSION}"
+
+# 解析版本号
+IFS='.' read -r major minor patch <<< "${CURRENT_VERSION}"
+
+# 根据规则递增版本号
+# 如果 patch 版本是 10，则将 minor 版本加 1，patch 重置为 0
+if [ "$patch" -eq "10" ]; then
+    minor=$((minor + 1))
+    patch=0
+else
+    patch=$((patch + 1))
+fi
+
+# 组合新版本号
+VERSION="v${major}.${minor}.${patch}"
 VERSION_WITHOUT_V="${VERSION#v}"
+echo "New version will be: ${VERSION}"
 
 # 检查是否有未提交的更改，如果有则提交
 if [[ -n $(git status -s) ]]; then
@@ -28,6 +45,9 @@ if [ -f "conda/meta.yaml" ]; then
     sed -i "s/{% set version = \".*\" %}/{% set version = \"${VERSION_WITHOUT_V}\" %}/" conda/meta.yaml
     git add conda/meta.yaml
 fi
+
+# 提交版本更新
+git commit -m "Bump version to ${VERSION}"
 
 # 推送更改
 git push origin main
