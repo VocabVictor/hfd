8165229813e6527f1adb46e053487e29cef5352e
migu <migu@example.com>
2025-01-13 02:14:43 +0000

    update something [skip actions]

--- test.sh
+++ test.sh
@@ -10,19 +10,6 @@ command -v twine >/dev/null 2>&1 || { echo "需要安装 twine，请运行: pip 
 # 获取当前版本号
 CURRENT_VERSION=$(cat Cargo.toml | grep '^version =' | sed 's/version = "\(.*\)"/\1/')
 
-# git 提交
-if [[ -n $(git status -s) ]]; then
-    git add .
-    git commit -m "update something [skip actions]"
-    git push
-else
-    echo "没有需要提交的更改"
-fi
-
-# 构建当前平台的 wheel
-echo "构建 wheel..."
-micromamba run -n base maturin build --release --skip-auditwheel
-
 # 获取最新构建的 wheel 文件
 LATEST_WHEEL=$(ls -t target/wheels/*.whl | head -n1)
 
@@ -31,3 +18,12 @@ micromamba run -n base pip uninstall hfd -y
 micromamba run -n base pip install "$LATEST_WHEEL"
 # micromamba run -n base hfd -h
 # micromamba run -n base hfd Wild-Heart/Disney-VideoGeneration-Dataset
+
+# git 提交
+if [[ -n $(git status -s) ]]; then
+    git add .
+    git commit -m "update something [skip actions]"
+    git push --force
+else
+    echo "没有需要提交的更改"
+fi
